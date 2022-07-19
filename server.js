c045d841039933ddc7e2c786e0c4a497feb50c60
Damien <damien.auversack@outlook.be>
2022-07-19 13:22:41 +0200

    minors

--- server.js
+++ server.js
@@ -83,6 +83,8 @@ let server = http.createServer(async (request, response) => {
 
   response.setHeader('Access-Control-Allow-Origin', 'https://damien-auversack.github.io');
   response.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  response.header('Access-Control-Allow-Credentials', true);
   response.writeHead(200, { "Content-Type": "text/plain" });
 
   response.end(JSON.stringify(scrapResultInfos));
